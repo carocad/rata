44be1902b108fa89cca9e3b3d89a362e97a68528
Camilo Roca <camilo.roca@cobi.bike>
2019-03-31 14:09:52 +0200

    example with official version

--- example/project.clj
+++ example/project.clj
@@ -6,7 +6,7 @@
   :min-lein-version "2.7.1"
 
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [hiposfer/rata "0.3.0-SNAPSHOT2"]
+                 [hiposfer/rata "0.3.0"]
                  [datascript "0.18.2"]
                  [org.clojure/clojurescript "1.10.238"]
                  [reagent "0.8.1"]]
